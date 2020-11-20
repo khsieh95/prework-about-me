9aa4315804c3091c348ad684cf010940525d968a
Kevin Hsieh <khsieh95@gmail.com>
2020-11-20 12:46:25 -0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,28 @@
+
+
+document.getElementById("grow").addEventListener("click",function(){
+
+        document.getElementById("box").style.height="200px"
+});
+
+document.getElementId ("blue").addEventListener ("click",function(){
+
+        document.getElementById("box") .style.color = "blue"
+});
+
+document.getElementById("reset").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px";
+
+});
+
+document.getElementById ("fade") .addEventListener ("click", function (){
+
+    document.getElementById ("box") .style.opacity = 1
+});
+
+document.getElementById ("shrink").addEventListener ("click", function(){
+
+        document.getElementById ("box") .style.height="50px"
+})
+
